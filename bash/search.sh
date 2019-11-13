1f6beec4cb984d0dacadf5f7c2f5bc5ba1912558
Tessey <danylo.zakharchenko@nure.ua>
2019-11-13 10:00:57 +0200

    Completed tasks

---
+++ bash/search.sh
@@ -0,0 +1,12 @@
+﻿#Do the script files searching from certain user.
+#!/bin/bash
+
+if [[ $# -eq 2 ]]
+then
+	find $1 -group $2
+elif [[ $# -eq 3 ]]
+then 
+	find $1 -group $2 -name "$3"
+else
+	echo "Ожидается 2 или 3 аргумента!"
+fi
