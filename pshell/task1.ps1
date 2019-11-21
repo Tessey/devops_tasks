46ab578f2ac55c9cd2ad3511d8a7b9e7f92b444b
Tessey <danylo.zakharchenko@nure.ua>
2019-11-21 13:25:22 +0200

    Task1 PowerShell

---
+++ pshell/task1.ps1
@@ -0,0 +1,43 @@
+﻿switch($args[0])
+{
+
+--users
+{
+    Get-LocalUser
+    break
+}
+
+--sessions
+{
+    query user /server:$SERVER
+    break
+}
+
+--processes
+{
+    try
+        {
+            $owners = @{}
+gwmi win32_process |% {$owners[$_.handle] = $_.getowner().user}
+
+get-process | select processname,Id,@{l="Owner";e={$owners[$_.id.tostring()]}}
+        }
+     catch
+        {
+             Write-Host Users flag are not specified
+        }
+}
+
+Get-Help
+{
+    Write-Host '
+--users: Display all users on your desctop (UserAccounts)
+--sessions - display all active sessions on local computer
+--processes - [ACCEPTABLE ONLY IF USERS FLAG SPECIFIED] get processes runned by --users
+               '
+}
+Default
+    {
+        Write-Host Something went wrong. Call the Get-Help command
+    }
+}
